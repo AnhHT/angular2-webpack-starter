ac3cf9e75fdc308d71a3a6a67b80f8ad0da6478f
Anh Hoang <anh.hta88@gmail.com>
2016-03-23 18:02:43 +0700

    anhht : update api
    Signed-off-by: Anh Hoang <anh.hta88@gmail.com>

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
 
 /*
  * We're loading this component asynchronously
@@ -10,11 +11,61 @@ console.log('`About` component loaded asynchronously');
 
 @Component({
   selector: 'about',
-  template: `patrick@AngularClass.com`
+  template: `<div class="tempView">
+    <table>
+      <thead>
+        <th>Name</th>
+        <th>Fullname</th>
+        <th>more_column</th>
+        <th>more_column2</th>
+        <th>more_column3</th>
+        <th>more_column4</th>
+        <th>more_column5</th>
+        <th>more_column6</th>
+        <th>more_column7</th>
+      </thead>
+      <tbody>        
+        <tr *ngFor="#item of data.result">
+          <td><span class="{{item.id % 2 ? 'even' : 'odd'}}">{{item.test_data}}</span></td>
+          <td>{{item.raw_data}}</td>
+          <td>{{item.more_column}}</td>
+          <td>{{item.more_column2}}</td>
+          <td>{{item.more_column3}}</td>
+          <td>{{item.more_column4}}</td>
+          <td>{{item.more_column5}}</td>
+          <td>{{item.more_column6}}</td>
+          <td>{{item.more_column7}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>`,
+  styles: [`div.tempView {
+  padding: 0px 20px;
+}
+
+  div.tempView span {
+    float:left;
+    padding: 5px;
+    margin: 2px;
+    display: block;
+  }
+
+  div.tempView span.odd {
+    background: red;
+    color: #fff;
+  }
+
+  div.tempView span.even {
+        background: deepskyblue;
+        color: #fff;
+  }`]
 })
 export class About {
-  constructor() {
-
+  data = {};
+  constructor(http: Http) {
+    http.get("http://localhost:59284/Home/GetData")
+    .map(res => res.json())
+    .subscribe(data => this.data = data);
   }
 
   ngOnInit() {
@@ -23,8 +74,8 @@ export class About {
     var mockData = require('assets/mock-data/mock-data.json');
     console.log('mockData', mockData);
     // if you're working with mock data you can also use http.get('assets/mock-data/mock-data.json')
-    this.asyncDataWithWebpack();
   }
+
   asyncDataWithWebpack() {
     // you can also async load mock data with 'es6-promise-loader'
     // you would do this if you don't want the mock-data bundled
